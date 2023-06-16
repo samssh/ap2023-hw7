6a3653f948e4e76ce0ffbe1d53e3b56d8af5ec11
seyed amir mohammad sadat shokouhi <sam.sadat.shokouhi@gmail.com>
2023-06-16 20:46:16 +0330

    add starter project

---
+++ src/ir/sharif/math/ap2023/hw7/ObjectMaker.java
@@ -0,0 +1,27 @@
+package ir.sharif.math.ap2023.hw7;
+
+
+import java.net.URL;
+import java.util.*;
+
+public class ObjectMaker {
+    private static final List<Class<?>> primitiveClasses = Arrays.asList(
+            boolean.class,
+            short.class,
+            byte.class,
+            int.class,
+            long.class,
+            float.class,
+            double.class,
+            String.class
+    );
+
+    public ObjectMaker(URL... urls) {
+        // TODO
+    }
+
+    public Object makeObject(Map<String, Object> values, String className) throws ReflectiveOperationException {
+        // TODO
+    }
+
+}
---
+++ src/ir/sharif/math/ap2023/hw7/SetValue.java
@@ -0,0 +1,12 @@
+package ir.sharif.math.ap2023.hw7;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(value = ElementType.FIELD)
+public @interface SetValue {
+    String path();
+}
